b7ad13e816fd822b23c5209f5dcad7fa6fd467c1
vivihuang <rwhuang@thoughtworks.com>
2016-03-31 16:58:06 +0800

    modify the timezone to current district as Asia/Shanghai

--- memos/models.py
+++ memos/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Memo(models.Model):
-    create_date = models.DateTimeField('date created')
+    create_date = models.DateTimeField(default=timezone.now)
     active_status = models.BooleanField(default=True)
     content = models.CharField(max_length=256)
