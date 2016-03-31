b156cc34e5c3b145e5f35873dcc2c5b51e4d5a5c
vivihuang <rwhuang@thoughtworks.com>
2016-03-31 17:33:24 +0800

    first test for memo model

--- memos/tests.py
+++ memos/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import Memo
 
-# Create your tests here.
+
+class MemoTestCase(TestCase):
+    def setUp(self):
+        self.content = 'First Item'
+        self.time = timezone.now()
+        Memo.objects.create(content=self.content, active_status=True, create_date=self.time)
+
+    def test_memos_can_save(self):
+        print(self.time)
+        item = Memo.objects.get(content=self.content)
+        self.assertEqual(item.id, 1)
+        self.assertEqual(item.active_status, True)
+        self.assertEqual(item.create_date, self.time)
