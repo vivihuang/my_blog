e15d75691d713661c5868e01a77873bd96a064bc
vivihuang <rwhuang@thoughtworks.com>
2016-03-31 16:16:14 +0800

    update memo model with boolean field for active_status

--- memos/models.py
+++ memos/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Memo(models.Model):
     create_date = models.DateTimeField('date created')
-    status = models.CharField(max_length=64)
+    active_status = models.BooleanField(default=True)
     content = models.CharField(max_length=256)
