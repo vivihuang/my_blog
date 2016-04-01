7078e7728b3be2df4e14d5420b34f74d1ebb93b6
vivihuang <rwhuang@thoughtworks.com>
2016-04-01 14:20:39 +0800

    modify all django render response to base.html

--- memos/views.py
+++ memos/views.py
@@ -1,12 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
-from .models import Memo
 
 
 def index(request):
-    memo_list = Memo.objects.all()
-    template = loader.get_template('memos/index.html')
-    context = {
-        'memo_list': memo_list,
-    }
-    return HttpResponse(template.render(context, request))
+    template = loader.get_template('base.html')
+    return HttpResponse(template.render(request))
